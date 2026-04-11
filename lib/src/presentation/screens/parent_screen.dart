61972bae584b836f60a2cc0b65a4b169800c7069
snaccbae74 <snaccbae74@gmail.com>
2026-04-11 11:27:00 +0200

    Added a log out button to parent_screen.dart

--- lib/src/presentation/screens/parent_screen.dart
+++ lib/src/presentation/screens/parent_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../src/bloc/auth/auth_bloc.dart';
+import '../../../src/bloc/auth/auth_event.dart';
 
 class ParentScreen extends StatelessWidget {
   final String fullName;
   const ParentScreen({super.key, required this.fullName});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome, $fullName')),
+      appBar: AppBar(
+        title: Text('Welcome, $fullName'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => context.read<AuthBloc>().add(LogoutRequested()),
+          ),
+        ],
+      ),
       body: Center(child: Text('Parent dashboard - implement features here')),
     );
   }
